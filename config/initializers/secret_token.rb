c15c5e9b047ab297f581f209cd4224a845db68fb
juanpabloe <juannpablo@gmail.com>
2011-10-03 23:00:49 -0500

    Monterrey Hackathon rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HackathonACM::Application.config.secret_token = '62f59e2b92593447f2766ae2d9a202dd88b5cb821d8027a5cdb0bf775f0b9f5e506795a5d2a0a676f6d6281bb0cc5d5f0d6ba385d964915cf8405362e5888204'
